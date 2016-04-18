49dbd8f15f073e77edaef09c90c2943b6b6c279e
adamShaleen <adamshaleen@gmail.com>
2016-04-17 20:20:18 -0600

    added nav button hidden functions and archer favicon

--- js/service.js
+++ js/service.js
@@ -27,6 +27,7 @@ this.loading = function() {
 this.loading();
 //-----------------------------------------------------------------
 
+//--------------Text box script-----------------------------------
 
 this.aboutText = "Hi! I'm Archer, and I'm an Australian cattle dog.  I was born on a farm in southern Utah, July 17th, 2015. I love to go on hikes with my mom and dad, and rampage in the snow!";
 this.mapText = "Here's a map of some of my favorite places.  There's dog parks, hiking trails, and my sister's house.  If you see me coming down the trail, get outta the way!";
@@ -40,3 +41,5 @@ this.getMapText = function() {
 };
 
 });
+
+//----------------------------------------------------------------
