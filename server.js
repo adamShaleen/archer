25d48592b6fcc6f7b0ce23423c59fe98dadde63a
adamShaleen <adamshaleen@gmail.com>
2016-06-07 22:12:38 -0600

    created server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+// listen
+app.listen(3000, function() {
+    console.log("Roasting on port 3000");
+});  // closing server tag
